eb03ffacfd5e2b22411720f798bec8fcc868aa6f
bambangsolehudin <bamsolotaku@gmail.com>
2019-06-11 20:23:45 +0700

    lat rest API

---
+++ application/models/mahasiswa_model.php
@@ -0,0 +1,31 @@
+<?php  
+
+class mahasiswa_model extends CI_model{
+	public function getMahasiswa($id = null)
+	{	
+		if ($id === null) {
+			return $this->db->get('mahasiswa')->result_array(); 
+		}else{
+			return $this->db->get_where('mahasiswa',['id' => $id])->result_array(); 
+		}
+		
+	}
+
+	public function deleteMahasiswa($id)
+	{
+		$this->db->delete('mahasiswa',['id'=>$id]);
+		return $this->db->affected_rows();
+	}
+
+	public function createMahasiswa($data)
+	{
+		$this->db->insert('mahasiswa',$data);
+		return $this->db->affected_rows();
+	}
+
+	public function updateMAhasiswa($data , $id)
+	{
+		$this->db->update('mahasiswa',$data,['id'=$id]);
+		return $this->db->affected_rows();
+	}
+}
